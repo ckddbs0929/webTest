cb5b0dac48e1cf4f286387f58b4e0592b7ad0f45
saemmilee <ckddbs0929@naver.com>
2021-09-17 16:54:06 +0900

    게시글 댓글 구현중

--- seong/src/main/java/Board/seong/controller/CommentController.java
+++ seong/src/main/java/Board/seong/controller/CommentController.java
@@ -8,11 +8,9 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,6 +20,25 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
+    @RequestMapping(value={"/comments", "/comments/{idx}"}, method={RequestMethod.POST, RequestMethod.PATCH})
+    public JsonObject registerComment(@PathVariable(value = "idx", required = false)
+                                      Long idx, @RequestBody final CommentDTO params){
+        JsonObject jsonObj = new JsonObject();
+        try {
+            if(idx!=null){
+                params.setIdx(idx);
+            }
+            boolean isRegistered = commentService.registerComment(params);
+            jsonObj.addProperty("String", isRegistered);
+        }
+        catch (DataAccessException e){
+            jsonObj.addProperty("message"," 데이터베이스 처리 과정에 문제가 발생");
+        }
+        catch (Exception e) {
+            jsonObj.addProperty("message", "시스템에 문제가 발생");
+        }
+        return jsonObj;
+    }
     @GetMapping(value = "/comments/{boardIdx}")
     public JsonObject getCommentList(@PathVariable("boardIdx")Long boardIdx, @ModelAttribute("params")CommentDTO params){
         JsonObject jsonObj = new JsonObject();
