15ca0607a3e319a1f641d28b7d736900c3d29b7e
saemmilee <ckddbs0929@naver.com>
2021-09-23 14:03:06 +0900

    댓글 수정,삭제 완료

--- seong/src/main/java/Board/seong/SeongApplication.java
+++ seong/src/main/java/Board/seong/SeongApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SeongApplication {
 
 	public static void main(String[] args) {
+		System.out.println("aaaaa");
 		SpringApplication.run(SeongApplication.class, args);
 	}
 
