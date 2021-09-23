440a33bf67188171bf57be9b5f3840f8ef4cfb0b
saemmilee <ckddbs0929@naver.com>
2021-09-23 11:32:18 +0900

    댓글 수정,삭제 완료

--- seong/src/main/java/Board/seong/controller/CommentController.java
+++ seong/src/main/java/Board/seong/controller/CommentController.java
@@ -39,6 +39,7 @@ public class CommentController {
         }
         return jsonObj;
     }
+
     @GetMapping(value = "/comments/{boardIdx}")
     public JsonObject getCommentList(@PathVariable("boardIdx")Long boardIdx, @ModelAttribute("params")CommentDTO params){
         JsonObject jsonObj = new JsonObject();
@@ -48,6 +49,22 @@ public class CommentController {
             Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new GsonLocalDateTimeAdapter()).create();
             JsonArray jsonArr = gson.toJsonTree(commentList).getAsJsonArray();
             jsonObj.add("commentList", jsonArr);
+        }
+        return jsonObj;
+    }
+
+    @DeleteMapping(value = "/comments/{idx}")
+    public JsonObject deleteComment(@PathVariable("idx")final Long idx){
+        JsonObject jsonObj = new JsonObject();
+        try {
+            boolean isDeleted = commentService.deleteComment(idx);
+            jsonObj.addProperty("result", isDeleted);
+        }
+        catch (DataAccessException e) {
+            jsonObj.addProperty("message","데이터베이스 처리 과정에 문제가 발생하였습니다.");
+        }
+        catch (Exception e) {
+            jsonObj.addProperty("message","시스템에 문제가 발생하였습니다.");
         }
         return jsonObj;
     }
